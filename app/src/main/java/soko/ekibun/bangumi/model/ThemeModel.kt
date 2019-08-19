72fc05c784e6fac8ed644cf68880906592aa83f6
ekibun <soekibun@gmail.com>
2019-08-19 19:15:56 +0800

    translucent navigation default

--- app/src/main/java/soko/ekibun/bangumi/model/ThemeModel.kt
+++ app/src/main/java/soko/ekibun/bangumi/model/ThemeModel.kt
@@ -39,24 +39,19 @@ class ThemeModel(context: Context){
             //(context.getSystemService(Context.UI_MODE_SERVICE) as UiModeManager).nightMode = if(night) UiModeManager.MODE_NIGHT_YES else UiModeManager.MODE_NIGHT_NO
         }
 
-        fun updateNavigationTheme(activity: Activity): Boolean {
-            return updateNavigationTheme(activity.window, activity)
+        fun updateNavigationTheme(activity: Activity) {
+            updateNavigationTheme(activity.window, activity, false)
         }
-        fun updateNavigationTheme(window: Window, context: Context): Boolean {
-            val translucent = PreferenceManager.getDefaultSharedPreferences(context).getBoolean("translucent_navigation", false)
-            if(translucent) {
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                val light = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO
-                if(Build.VERSION.SDK_INT >= 26 && light) window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
-                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-                val color =  ResourceUtil.resolveColorAttr(context, android.R.attr.colorBackground)
-                window.navigationBarColor = Color.argb( 200, Color.red(color), Color.green(color), Color.blue(color))
-            }
-            else {
-                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                window.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            }
-            return translucent
+        fun updateNavigationTheme(window: Window, context: Context, dialog: Boolean = true) {
+            window.decorView.systemUiVisibility =  View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    (if(Build.VERSION.SDK_INT >= 26) View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION else 0) or
+                    (if(dialog) View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN else 0)
+            if(Build.VERSION.SDK_INT < 26) return
+            val light = AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_NO
+            if(light) window.decorView.systemUiVisibility = window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            val color =  ResourceUtil.resolveColorAttr(context, android.R.attr.colorBackground)
+            window.navigationBarColor = Color.argb( 200, Color.red(color), Color.green(color), Color.blue(color))
         }
     }
 }
