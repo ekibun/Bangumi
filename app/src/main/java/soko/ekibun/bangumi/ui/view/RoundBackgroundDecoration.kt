ed227762f0ff49310a7cdf99ef2a664a768f9811
ekibun <soekibun@gmail.com>
2020-05-02 18:34:45 +0800

    fix decoration blank in night mode

--- app/src/main/java/soko/ekibun/bangumi/ui/view/RoundBackgroundDecoration.kt
+++ app/src/main/java/soko/ekibun/bangumi/ui/view/RoundBackgroundDecoration.kt
@@ -1,17 +1,19 @@
 package soko.ekibun.bangumi.ui.view
 
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import soko.ekibun.bangumi.App
 import soko.ekibun.bangumi.R
 import soko.ekibun.bangumi.util.ResourceUtil
 
 class RoundBackgroundDecoration(val offset: Int = 0) : RecyclerView.ItemDecoration() {
-    private val roundCornerDrawable by lazy { App.app.getDrawable(R.drawable.bg_round_dialog) }
+    private lateinit var roundCornerDrawable: Drawable
     private val dp24 = ResourceUtil.toPixels(24f)
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        if (!::roundCornerDrawable.isInitialized)
+            roundCornerDrawable = parent.context.getDrawable(R.drawable.bg_round_dialog)!!
         val layoutManager = (parent.layoutManager as? LinearLayoutManager) ?: return
         val scrollStart = Math.min(layoutManager.findViewByPosition(0)?.let {
             -layoutManager.getDecoratedTop(it) - offset
